dc1438d41df97586dacd9535e51eb1784eff02cb
cat-k <cat-kotov@yandex.ru>
2022-05-21 00:31:12 +0300

    project initialisation

---
+++ src/main/java/org_cat/eye/familybudget/user/FamilybudgetUserApplication.java
@@ -0,0 +1,13 @@
+package org_cat.eye.familybudget.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilybudgetUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilybudgetUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org_cat/eye/familybudget/user/FamilybudgetUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org_cat.eye.familybudget.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilybudgetUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
