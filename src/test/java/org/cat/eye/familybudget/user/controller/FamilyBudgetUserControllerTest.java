78f9d937112d38ada5e07e1dd37ad24ec10bfbb4
akotov <cat-kotov@yandex.ru>
2022-05-24 19:28:02 +0300

    Rest controller test was added.

--- src/test/java/org/cat/eye/familybudget/user/controller/FamilyBudgetUserControllerTest.java
+++ src/test/java/org/cat/eye/familybudget/user/controller/FamilyBudgetUserControllerTest.java
@@ -4,7 +4,6 @@ import org.cat.eye.familybudget.user.model.FamilyBudgetUser;
 import org.cat.eye.familybudget.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
