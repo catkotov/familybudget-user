b37fc9c5d7196aef3c93d4f501d97d9696faa087
cat-k <cat-kotov@yandex.ru>
2022-05-29 07:40:26 +0300

    UserRepository test was added.

--- src/test/java/org/cat/eye/familybudget/user/repository/UserRepositoryTest.java
+++ src/test/java/org/cat/eye/familybudget/user/repository/UserRepositoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -16,8 +19,18 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository repository;
 
+    @BeforeEach
+    void init() {
+
+    }
+
+    @AfterEach
+    void clean() {
+        repository.deleteAll();
+    }
+
     @Test
-    void save() {
+    void saveAndFindById() {
         FamilyBudgetUser expectedUser = new FamilyBudgetUser();
         expectedUser.setNicName("first");
         expectedUser.setPassword("bla-bla-bla");
@@ -26,6 +39,28 @@ class UserRepositoryTest {
         FamilyBudgetUser user = repository.findById("first").orElseGet(FamilyBudgetUser::new);
         assertEquals("first", user.getNicName());
         assertEquals("bla-bla-bla", user.getPassword());
+    }
+
+    @Test
+    void saveAllAndFindAll() {
+
+        List<FamilyBudgetUser> users = new LinkedList<>();
+
+        FamilyBudgetUser user_1 = new FamilyBudgetUser();
+        user_1.setNicName("first");
+        user_1.setPassword("bla-bla-bla");
+        users.add(user_1);
+        FamilyBudgetUser user_2 = new FamilyBudgetUser();
+        user_2.setNicName("second");
+        user_2.setPassword("blu-blu-blu");
+        users.add(user_2);
+
+        repository.saveAll(users);
+
+        List<FamilyBudgetUser> userList = (List<FamilyBudgetUser>) repository.findAll();
+
+        assertEquals(2, userList.size());
+        assertEquals(2, repository.count());
     }
 
 }
